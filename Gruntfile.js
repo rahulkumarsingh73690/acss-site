0b6c207e9f226ed52dc9aed9b1a83ae1d6306a0f
Thierry <thierryk@yahoo-inc.com>
2015-04-02 22:17:03 -0700

    Add sub-directories to what files to check

--- Gruntfile.js
+++ Gruntfile.js
@@ -147,7 +147,7 @@ module.exports = function(grunt) {
                 },
                 files: [
                     {
-                        src: ['./app/components/*.jsx', './app/docs/*.md'],
+                        src: ['./app/components/*.jsx', './app/docs/**/*.md'],
                         dest: '<%= project.build %>/css/atomic.css'
                     }
                 ]
