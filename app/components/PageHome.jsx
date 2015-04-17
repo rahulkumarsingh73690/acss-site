ddb5f00065095b2a0c3951731ee8b33568ec2856
Thierry <thierryk@yahoo-inc.com>
2015-04-17 10:49:43 -0700

    Fix border color

--- app/components/PageHome.jsx
+++ app/components/PageHome.jsx
@@ -27,14 +27,14 @@ var PageHome = React.createClass({
     render: function() {
         return (
             <div className="home-page">
-                <div id='splash' className='D(tb) W(100%) BdB'>
+                <div id='splash' className='D(tb) W(100%) BdB Bdc(#0280ae.3)'>
                     <div className='D(tbc) Ta(c) Va(m)'>
                         <h1 className='Mx(a) W(50%) Pos(r) Ov(h)'>
                             <img id="logo" className='Start(0) Pos(a) Pos(r)--sm' alt='atomic css' src={assets['images/atomic-css-logo.svg']} />
                         </h1>
                         <p className='My(20px)'>A collection of single purpose styling units for maximum reuse.</p>
                         <p>
-                            <NavLink className='D(ib) Mb(10px) P(20px) C(#fff) Bgc(logo) Bdrs(100px) Td(n):h' routeName='docs' navParams={{key: 'quick-start'}}>Get Started</NavLink>
+                            <NavLink className='D(ib) Mb(10px) P(20px) C(#fff) Bgc($brandColor) Bdrs(100px) Td(n):h' routeName='docs' navParams={{key: 'quick-start'}}>Get Started</NavLink>
                         </p>
                         <p>Build whatever you want, the way you want it.</p>
                     </div>
