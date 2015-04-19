0807a0967659ef38f2eb554d5bc8c2dd9a7ea292
Thierry <thierryk@yahoo-inc.com>
2015-04-19 11:54:04 -0700

    New (png) logo

--- app/components/PageHome.jsx
+++ app/components/PageHome.jsx
@@ -30,7 +30,7 @@ var PageHome = React.createClass({
                 <div id='splash' className='D(tb) W(100%) BdB Bdc(#0280ae.3) Bgc($brandColor)'>
                     <div className='D(tbc) Ta(c) Va(m)'>
                         <h1 className='Mx(a) W(50%) Pos(r) Ov(h)'>
-                            <img id="logo" className='Start(0) Pos(a) Pos(r)--sm' alt='atomic css' src={assets['images/atomic-css-logo.svg']} />
+                            <img id="logo" alt='atomic css' src={assets['images/atomic-css-logo.png']} />
                         </h1>
                         <p>
                             <NavLink className='M(20px) D(ib) Py(10px) Px(20px) Fz(20px) C(#fff) Bgc(#fff.3) Bdrs(2px) Bxsh(light) Tsh(1) Fw(b) Td(n):h' routeName='docs' navParams={{key: 'quick-start'}}>Get Started</NavLink>
