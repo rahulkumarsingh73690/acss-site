e589c578a1537a4af3c231c50bf7175dfc987688
Thierry <thierryk@yahoo-inc.com>
2015-04-24 13:24:32 -0700

    Remove ID for 404 and 500 pages

--- app/components/Status500.jsx
+++ app/components/Status500.jsx
@@ -9,7 +9,7 @@ import {NavLink} from 'flux-router-component';
 class Status500 extends React.Component {
     render() {
         return (
-            <div id="500" className="innerwrapper Bxz(bb) Pt(40px) Px(10px) Mb(50px) Mx(a)--sm Maw(1000px)--sm W(90%)--sm W(a)">
+            <div className="innerwrapper Bxz(bb) Pt(40px) Px(10px) Mb(50px) Mx(a)--sm Maw(1000px)--sm W(90%)--sm W(a)">
                 <h1>Error</h1>
                 <p>Sorry there was an unexpected errror.</p>
                 <p><NavLink routeName="home">Back to the home page.</NavLink></p>
